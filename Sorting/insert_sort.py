49811158883b7cda4bc1f8bc5339174ecfb302f0
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-04-28 22:38:59 +0530

    WIP

--- Sorting/insert_sort.py
+++ Sorting/insert_sort.py
@@ -3,9 +3,15 @@ array = [27, 74, 5, 11, 17, 3, 62, 44, 31, 77]
 def insert_sort(array):
     for i in range(1, len(array)):
         key = array[i]
+        print(key)
         j = i - 1
         while j >= 0 and array[j] > key:
             array[j + 1] = array[j]
             j -= 1
+            print(array)
+
         array[j + 1] = key
+        print(array)
     return array
+
+print(insert_sort(array))
